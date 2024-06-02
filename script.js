510ef00cc39339fa5548083e7ff2cc5914c0898c
Silkdigitalofficial <pandukasl13@gmail.com>
2024-06-02 18:29:48 +0530

    update

---
+++ script.js
@@ -0,0 +1,60 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+
+
+
+
+
+let section = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a');
+
+
+window.onscroll = () => {
+    section.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetup - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navlinks.forEach(link => {
+                links.classlist.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+
+        }
+    });
+
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove("active");
+};
+
+ScrollReveal({
+    //reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+ });
+
+ScrollReveal().reveal('.home-contact , .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img , .service-container, .portofolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-contact h1 , .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-contact p, .about-content', { origin: 'right' });
+
+const typed = new Typed('.multiple-text', {
+    strings: ['Creative Designer', 'Concept Developer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
